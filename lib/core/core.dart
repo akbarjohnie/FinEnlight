e816baee7a64b0a8f251803e95beaaccc33d6fb0
Akbar Rashidov <a.jon.rashidov@gmail.com>
2025-09-27 14:17:41 +0300

    build: update app_colors names & update core

--- lib/core/core.dart
+++ lib/core/core.dart
@@ -1,3 +1,4 @@
 export 'di/di.dart';
 export 'utils/utils.dart';
 export 'widgets/widgets.dart';
+export 'theme/theme.dart';
--- lib/core/theme/app_colors.dart
+++ lib/core/theme/app_colors.dart
@@ -2,26 +2,31 @@ import 'dart:ui';
 
 abstract base class AppColors {
   static const Color white = Color(0xFFFFFFFF);
-  static const Color appBlack = Color(0xFF1C1C1C);
+  static const Color eerieBlack = Color(0xFF1C1C1C);
 
-  static const Color gray = Color(0xFFEAEAEA);
-  static const Color gray_2 = Color(0xFFF8F8F8);
-  static const Color gray_3 = Color(0xFFF1F1F1);
-  static const Color gray_4 = Color(0xFFEFEFEF);
-  static const Color darkGray = Color(0xFF868686);
+  /// [GRAY]
+  static const Color platinum = Color(0xFFEAEAEA);
+  static const Color alabaster = Color(0xFFF8F8F8);
+  static const Color gainsboro = Color(0xFFF1F1F1);
+  static const Color galleryGray = Color(0xFFEFEFEF);
+  static const Color dimGray = Color(0xFF868686);
 
-  static const Color appBlue = Color(0xFF537CE8);
-  static const Color skyBlue = Color(0xFFEEF2FD);
+  /// [BLUE]
+  static const Color cornflowerBlue = Color(0xFF537CE8);
+  static const Color aliceBlue = Color(0xFFEEF2FD);
 
-  static const Color appRed = Color(0xFFFC6B58);
+  /// [RED]
+  static const Color bittersweet = Color(0xFFFC6B58);
 
-  static const Color palePink = Color(0xFFFFF0EE);
-  static const Color appYellow = Color(0xFFFFD922);
+  static const Color linen = Color(0xFFFFF0EE);
 
-  static const Color paleYellow = Color(0xFFFFFBE9);
+  /// [YELLOW]
+  static const Color sunglow = Color(0xFFFFD922);
 
-  static final g2opacity_50 = gray_2.withValues(alpha: 0.50);
-  static final appBlueOpacity_10 = appBlue.withValues(alpha: 0.10);
-  static final appRedOpacity_10 = appRed.withValues(alpha: 0.10);
-  static final appYellowOpacity_10 = appYellow.withValues(alpha: 0.10);
+  static const Color oldLace = Color(0xFFFFFBE9);
+
+  static final alabasterOpacity50 = alabaster.withValues(alpha: 0.50);
+  static final cornflowerBlueOpacity10 = cornflowerBlue.withValues(alpha: 0.10);
+  static final bittersweetOpacity10 = bittersweet.withValues(alpha: 0.10);
+  static final sunglowOpacity10 = sunglow.withValues(alpha: 0.10);
 }
