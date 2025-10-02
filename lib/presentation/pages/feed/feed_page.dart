0a7791b39eb258b3ee4e7bd3e50e9b9127a7ffec
Argentum-D <pavel_106@bk.ru>
2025-10-02 20:12:58 +0300

    initial: start feed UI

--- lib/presentation/pages/feed/feed_page.dart
+++ lib/presentation/pages/feed/feed_page.dart
@@ -1,4 +1,5 @@
 import 'package:auto_route/auto_route.dart';
+import 'package:fin_enlight/core/core.dart';
 import 'package:flutter/material.dart';
 
 @RoutePage()
@@ -7,8 +8,22 @@ class FeedPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return const Scaffold(
       backgroundColor: Colors.red,
+      body: SafeArea(
+        child: CustomScrollView(
+          slivers: [
+            SliverAdapter(
+              padding: P(
+                horizontal: 16,
+                top: 16,
+                bottom: 12,
+              ),
+              child: Row(),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
